9c35157abce3e34113372a212348562ea2b720ab
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-02-18 23:29:36 +0100

    Postgres DB connection setup

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::CustomError;
+use sqlx::PgPool;
+use dotenv::dotenv;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -8,6 +11,15 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+
+    dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL is not set in environmental variables!");   
+
+    let db_connetion = PgPool::connect(&db_url)
+        .await
+        .map_err(CustomError::new)?;
+
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
